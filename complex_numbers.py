aec978edad0565f31161cfa896b0e975b9e81b1b
KlimVladMaks <vkm.blog@yandex.ru>
2023-01-09 22:23:52 +0300

    Update #3

--- complex_numbers.py
+++ complex_numbers.py
@@ -1,10 +1,13 @@
+import copy
+import typing as tp
+
+
 class ComplexNumber:
     """
-    Класс для реализации комплексного числа.
+    Класс для реализации комплексного числа и сопутствующих функций.
     """
 
-    # TODO: Решить проблему, что при изменении re_z и im_z, pair_form остаётся прежней
-    def __init__(self, re_z: int = 0, im_z: int = 0):
+    def __init__(self, re_z: tp.Union[int, float] = 0, im_z: tp.Union[int, float] = 0):
         """
         Инициализируем комплексное число при создании экземпляра класса.
         :param re_z: Вещественная часть числа.
@@ -12,15 +15,19 @@ class ComplexNumber:
         """
 
         # Проверяем, что введены числовые коэффициенты, и если это не так, то выбрасываем ошибку
-        if (type(re_z) is not int) or (type(im_z) is not int):
+        if not(isinstance(re_z, (int, float))) or not(isinstance(im_z, (int, float))):
             raise Exception("Введены нечисловые коэффициенты для комплексного числа")
 
         # Задаём вещественную и мнимую часть комплексного числа
         self.re_z = re_z
         self.im_z = im_z
 
-        # Задаём вид комплексного числа в формате пары чисел
-        self.pair_form = (re_z, im_z)
+    def get_pair_form(self) -> tuple[tp.Union[int, float], tp.Union[int, float]]:
+        """
+        Функция для получения комплексного числа в формате картежа из двух чисел.
+        :return: Картеж из двух чисел - вещественной и мнимой части.
+        """
+        return self.re_z, self.im_z
 
     @staticmethod
     def is_equal(num_1: 'ComplexNumber', num_2: 'ComplexNumber', *nums: 'ComplexNumber') -> bool:
@@ -67,17 +74,16 @@ class ComplexNumber:
         if not(ComplexNumber.is_complex_number(num_1, num_2, *nums)):
             raise Exception("Функция ComplexNumber.sum() работает только с объектами класса ComplexNumber")
 
-        # Создаём нулевые переменные для вещественной и мнимой части
-        re_z = 0
-        im_z = 0
+        # Создаём нулевое комплексное число
+        num_sum = ComplexNumber()
 
-        # Перебираем все переданные числа, складывая их вещественные и мнимые части с созданными выше
+        # Перебираем все переданные числа, складывая их вещественные и мнимые части с таковыми у созданного выше числа
         for num in [num_1, num_2, *nums]:
-            re_z += num.re_z
-            im_z += num.im_z
+            num_sum.re_z += num.re_z
+            num_sum.im_z += num.im_z
 
         # Возвращаем комплексное число с вещественной и мнимой частями, полученными в результате сложения
-        return ComplexNumber(re_z, im_z)
+        return num_sum
 
     @staticmethod
     def is_complex_number(*nums) -> bool:
@@ -94,6 +100,94 @@ class ComplexNumber:
                 return False
         return True
 
+    @staticmethod
+    def mul(num_1: 'ComplexNumber', num_2: 'ComplexNumber', *nums: 'ComplexNumber') -> 'ComplexNumber':
+        """
+        Функция для перемножения нескольких комплексных чисел.
+        :param num_1: Первое комплексное число.
+        :param num_2: Второе комплексное число.
+        :param nums: Дополнительные комплексные числа.
+        :return: Комплексное число, являющееся результатом перемножения заданных комплексных чисел.
+        """
 
+        # Если переданные объекты не являются комплексными числами, то выбрасываем ошибку
+        if not (ComplexNumber.is_complex_number(num_1, num_2, *nums)):
+            raise Exception("Функция ComplexNumber.mul() работает только с объектами класса ComplexNumber")
+
+        # За изначальное значение берём первое переданное число (копируем его)
+        num_mul = copy.deepcopy(num_1)
+
+        # Перебираем все оставшиеся комплексные числа и поочерёдно перемножаем их с изначальным
+        for num in [num_2, *nums]:
+            a = num_mul.re_z
+            b = num_mul.im_z
+            c = num.re_z
+            d = num.im_z
+            num_mul.re_z = (a * c) - (b * d)
+            num_mul.im_z = (a * d) + (b * c)
+
+        # Возвращаем комплексное число, являющееся произведением все переданных чисел
+        return num_mul
+
+    def get_opposite_by_sum(self) -> 'ComplexNumber':
+        """
+        Функция, возвращающая обратное комплексное число по сумме.
+        :return: Обратное комплексное число по сумме.
+        """
+        return ComplexNumber(-self.re_z, -self.im_z)
+
+    def get_opposite_by_mul(self) -> 'ComplexNumber':
+        """
+        Функция, возвращающая обратное комплексное число по произведению.
+        :return: Обратное комплексное число по произведению.
+        """
+
+        # Находим обратное комплексное число по формуле и возвращаем его
+        a = self.re_z
+        b = self.im_z
+        a_opposite = a / ((a ** 2) + (b ** 2))
+        b_opposite = -b / ((a ** 2) + (b ** 2))
+        return ComplexNumber(a_opposite, b_opposite)
+        pass
+
+    def get_conjugate(self) -> 'ComplexNumber':
+        """
+        Функция, возвращающая сопряжённое комплексное число.
+        :return: Сопряжённое комплексное число.
+        """
+        return ComplexNumber(self.re_z, -self.im_z)
+
+    @staticmethod
+    def div(num_1: 'ComplexNumber', num_2: 'ComplexNumber') -> 'ComplexNumber':
+        """
+        Функция для деления одного комплексного числа на другое.
+        :param num_1: Делимое (комплексное число).
+        :param num_2: Делитель (комплексное число).
+        :return: Результат деления (комплексное число).
+        """
+
+        # Если переданные объекты не являются комплексными числами, то выбрасываем ошибку
+        if not (ComplexNumber.is_complex_number(num_1, num_2)):
+            raise Exception("Функция ComplexNumber.div() работает только с объектами класса ComplexNumber")
+
+        # Если делитель равен нулю, то выводим сообщение об ошибке
+        if num_2.get_pair_form() == (0, 0):
+            raise Exception("Функция ComplexNumber.div() на способно делить на ноль")
+
+        # Преобразуем числитель и знаменатель получившийся дроби
+        numerator = ComplexNumber.mul(num_1, num_2.get_conjugate())
+        denominator = ComplexNumber.mul(num_2, num_2.get_conjugate())
+
+        # Вытаскиваем из знаменателя вещественное число (т.к. мнимая часть равно нулю)
+        denominator = denominator.re_z
+
+        # Преобразуем знаменатель в комплексное число вида (1/re_z, 0)
+        denominator = ComplexNumber(1 / denominator, 0)
+
+        # Умножаем числитель на преобразованный знаменатель
+        num_div = ComplexNumber.mul(numerator, denominator)
+
+        # Возвращаем получившееся значение
+        return num_div
 
 
--- unit_tests.py
+++ unit_tests.py
@@ -4,7 +4,7 @@ from complex_numbers import ComplexNumber
 
 class ComplexNumberTestCase(unittest.TestCase):
     """
-    Тесты для класса ComplexNumber.
+    Юнит-тесты для класса ComplexNumber.
     """
 
     def test_init(self):
@@ -20,12 +20,17 @@ class ComplexNumberTestCase(unittest.TestCase):
         self.assertEqual(cn_1.re_z, 1)
         self.assertEqual(cn_1.im_z, -5)
 
-        cn_1 = ComplexNumber(-7, 2)
+        cn_1 = ComplexNumber(-7)
         self.assertEqual(cn_1.re_z, -7)
-        self.assertEqual(cn_1.im_z, 2)
+        self.assertEqual(cn_1.im_z, 0)
+
+    def test_get_pair_form(self):
+
+        cn_1 = ComplexNumber()
+        self.assertEqual(cn_1.get_pair_form(), (0, 0))
 
         cn_1 = ComplexNumber(6, -10)
-        self.assertEqual(cn_1.pair_form, (6, -10))
+        self.assertEqual(cn_1.get_pair_form(), (6, -10))
 
     def test_is_complex_number(self):
 
@@ -76,12 +81,116 @@ class ComplexNumberTestCase(unittest.TestCase):
         cn_1 = ComplexNumber(-6, -3)
         cn_2 = ComplexNumber(7, 1)
         cn_result = ComplexNumber.sum(cn_1, cn_2)
-        self.assertEqual(cn_result.pair_form, (1, -2))
+        self.assertEqual(cn_result.get_pair_form(), (1, -2))
 
         cn_list: list[ComplexNumber] = []
         for i in range(10):
             cn_list.append(ComplexNumber(5, -3))
         cn_result = ComplexNumber.sum(*cn_list)
-        self.assertEqual(cn_result.pair_form, (50, -30))
+        self.assertEqual(cn_result.get_pair_form(), (50, -30))
+
+    def test_mul(self):
+
+        cn_1 = ComplexNumber(4, 2)
+        cn_2 = ComplexNumber(-10, -4)
+        num_1 = (5, 2)
+        self.assertRaises(Exception, ComplexNumber.sum, cn_1, cn_2, num_1)
+        self.assertRaises(Exception, ComplexNumber.is_equal, cn_1, num_1, cn_2)
+
+        cn_1 = ComplexNumber()
+        cn_2 = ComplexNumber(10, -6)
+        cn_result = ComplexNumber.mul(cn_1, cn_2)
+        self.assertEqual(cn_result.get_pair_form(), (0, 0))
+
+        cn_1 = ComplexNumber(5, -2)
+        cn_2 = ComplexNumber(10, -6)
+        cn_3 = ComplexNumber(6, 1)
+        cn_4 = ComplexNumber()
+        cn_result = ComplexNumber.mul(cn_1, cn_2, cn_3, cn_4)
+        self.assertEqual(cn_result.get_pair_form(), (0, 0))
+
+        cn_1 = ComplexNumber(2, 3)
+        cn_2 = ComplexNumber(-3, 2)
+        cn_3 = ComplexNumber(1, -1)
+        cn_4 = ComplexNumber(2, 2)
+        cn_result = ComplexNumber.mul(cn_1, cn_2, cn_3, cn_4)
+        self.assertEqual(cn_result.get_pair_form(), (-48, -20))
+
+    def test_get_opposite_by_sum(self):
+
+        cp_1 = ComplexNumber()
+        cp_result = cp_1.get_opposite_by_sum()
+        self.assertEqual(cp_result.get_pair_form(), (0, 0))
+
+        cp_1 = ComplexNumber(4, -3)
+        cp_result = cp_1.get_opposite_by_sum()
+        self.assertEqual(cp_result.get_pair_form(), (-4, 3))
+
+        cp_1 = ComplexNumber(5, -20)
+        cp_2 = cp_1.get_opposite_by_sum()
+        cp_result = ComplexNumber.sum(cp_1, cp_2)
+        self.assertEqual(cp_result.get_pair_form(), (0, 0))
+
+    def test_get_opposite_by_mul(self):
+
+        cp_1 = ComplexNumber(5, -13)
+        cp_2 = cp_1.get_opposite_by_mul()
+        cp_result = ComplexNumber.mul(cp_1, cp_2)
+        self.assertEqual(cp_result.get_pair_form(), (1, 0))
+
+    def test_get_conjugate(self):
+
+        cn_1 = ComplexNumber(7, -3)
+        cn_2 = cn_1.get_conjugate()
+        cn_result = ComplexNumber.mul(cn_1, cn_2)
+        answer = (cn_1.re_z ** 2) + (cn_1.im_z ** 2)
+        self.assertEqual(cn_result.get_pair_form(), (answer, 0))
+
+        cn_1 = ComplexNumber(5, 20)
+        cn_2 = cn_1.get_conjugate()
+        cn_3 = cn_2.get_conjugate()
+        self.assertEqual(cn_1.get_pair_form() == cn_3.get_pair_form(), True)
+
+        cn_1 = ComplexNumber(-11, 5)
+        cn_2 = ComplexNumber(-4, -7)
+        cn_1_2_sum = ComplexNumber.sum(cn_1, cn_2)
+        cn_1_conj = cn_1.get_conjugate()
+        cn_2_conj = cn_2.get_conjugate()
+        self.assertEqual(
+            cn_1_2_sum.get_conjugate().get_pair_form() == ComplexNumber.sum(cn_1_conj, cn_2_conj).get_pair_form(),
+            True
+        )
+
+        cn_1 = ComplexNumber(4, -20)
+        cn_2 = ComplexNumber(9, 6)
+        cn_1_2_mul = ComplexNumber.mul(cn_1, cn_2)
+        cn_1_conj = cn_1.get_conjugate()
+        cn_2_conj = cn_2.get_conjugate()
+        self.assertEqual(
+            cn_1_2_mul.get_conjugate().get_pair_form() == ComplexNumber.mul(cn_1_conj, cn_2_conj).get_pair_form(),
+            True
+        )
+
+    def test_div(self):
+
+        cn_1 = ComplexNumber(7, -3)
+        num_1 = (6, 8)
+        self.assertRaises(Exception, ComplexNumber.div, cn_1, num_1)
+        self.assertRaises(Exception, ComplexNumber.div, num_1, cn_1)
+
+        cn_1 = ComplexNumber(6, -3)
+        cn_2 = ComplexNumber()
+        self.assertRaises(Exception, ComplexNumber.div, cn_1, cn_2)
+
+        cn_1 = ComplexNumber(-1, 3)
+        cn_2 = ComplexNumber(1, 2)
+        cn_result = ComplexNumber.div(cn_1, cn_2)
+        self.assertEqual(cn_result.get_pair_form(), (1, 1))
+
+        cn_1 = ComplexNumber(13, 1)
+        cn_2 = ComplexNumber(7, -6)
+        cn_result = ComplexNumber.div(cn_1, cn_2)
+        self.assertEqual(cn_result.get_pair_form(), (1, 1))
+
 
 
